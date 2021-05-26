7a8af82b7dd2f5c264c248e76f7ec7dfa1ef57ec
mapleleafjack <jack.musajo@madetech.com>
2021-05-26 15:03:25 +0100

    Maintenance mode removal
    Removing remaining bits from maintenance mode

--- components/Layout/Header/Header.tsx
+++ components/Layout/Header/Header.tsx
@@ -73,23 +73,17 @@ const HeaderComponent = ({
               )}
             </a>
           </div>
-
-          {NEXT_PUBLIC_MAINTENANCE_MODE &&
-          NEXT_PUBLIC_MAINTENANCE_MODE === '1' ? (
-            ''
-          ) : (
-            <nav className="lbh-header__links" aria-label="Navigation menu">
-              {navLinks && (
-                <>
-                  {navLinks.map(({ name, path }) => (
-                    <Link href={path} key={path}>
-                      <a className="govuk-header__link">{name}</a>
-                    </Link>
-                  ))}
-                </>
-              )}
-            </nav>
-          )}
+          <nav className="lbh-header__links" aria-label="Navigation menu">
+            {navLinks && (
+              <>
+                {navLinks.map(({ name, path }) => (
+                  <Link href={path} key={path}>
+                    <a className="govuk-header__link">{name}</a>
+                  </Link>
+                ))}
+              </>
+            )}
+          </nav>
         </div>
       </div>
     </header>
